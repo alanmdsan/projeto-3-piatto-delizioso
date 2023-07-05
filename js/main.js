0a575b061407f6476b13381eaf58398d2d62863b
alanmdsan <alanmdsan@gmail.com>
2023-07-04 22:39:00 -0300

    set jquery validation

--- js/main.js
+++ js/main.js
@@ -2,5 +2,36 @@ $(document).ready(function() {
     
     $('#telefone').mask('(00) 00000-0000');
 
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: false
+            },
+            mensagem: {
+                required: true
+            }
+        },
+        messages: {
+            nome: '<span class="error-message">Por favor, insira o seu nome</span>',
+            email: '<span class="error-message">Por favor, insira o seu email corretamente</span>',
+            mensagem: '<span class="error-message">Por favor, insira sua mensagem</span>'
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let campos_incorretos = validador.numberOfInvalids();
+            if (campos_incorretos) {
+                // alert(`Existe(m) ${campos_incorretos} campo(s) incorreto(s)`);
+            }
+        }
+    });
 
 })
